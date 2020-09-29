0a1b3cd1032fee4ecffce680880ff572f020a811
Husnul Hidayat <husnultamvan@gmail.com>
2020-09-30 00:39:24 +0700

    Create generate-creds-from-secret.py

---
+++ generate-creds-from-secret.py
@@ -0,0 +1,14 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+gauth = GoogleAuth()
+gauth.LoadCredentialsFile("mycreds.txt")
+if gauth.credentials is None:
+    gauth.LocalWebserverAuth()
+elif gauth.access_token_expired:
+    gauth.Refresh()
+else:
+    gauth.Authorize()
+gauth.SaveCredentialsFile("mycreds.txt")
+
+drive = GoogleDrive(gauth)
